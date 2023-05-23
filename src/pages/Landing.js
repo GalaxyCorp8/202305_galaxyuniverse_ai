95bac4bb09c593e1283d1e356c84633d44d0180d
Dong-Hyun Ahn <yachtahn@gmail.com>
2023-05-23 23:53:19 +0900

    세번째 전달 소스 3번째 커밋, 영상 링크를  s3로

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,9 @@
-import Bg from '../assets/image/landing/landing_bg.png'
-import styled from 'styled-components'
-import React, { useEffect } from 'react'
-import { Parallax } from 'react-scroll-parallax'
-import { osName } from 'react-device-detect'
-import video from '../assets/video/main.webm'
+import Bg from "../assets/image/landing/landing_bg.png";
+import styled from "styled-components";
+import React, { useEffect } from "react";
+import { Parallax } from "react-scroll-parallax";
+import { osName } from "react-device-detect";
+// import video from '../assets/video/main.webm'
 
 const VideoBackground = styled.video`
 	position: absolute;
@@ -12,7 +12,7 @@ const VideoBackground = styled.video`
 	width: 100%;
 	height: 100%;
 	object-fit: cover;
-`
+`;
 const VideoWrapper = styled.div`
 	position: relative;
 	height: 100vh;
@@ -24,7 +24,7 @@ const VideoWrapper = styled.div`
 	.main-visual-inner {
 		z-index: 1;
 	}
-`
+`;
 
 const PostContainer = styled.div`
 	position: absolute;
@@ -35,14 +35,14 @@ const PostContainer = styled.div`
 	background-image: url(${Bg});
 	background-size: cover;
 	background-position: center;
-`
+`;
 
 const Landing = () => {
-	const [isMobile, setIsMobile] = React.useState(false)
+	const [isMobile, setIsMobile] = React.useState(false);
 	useEffect(() => {
-		console.log(osName)
-		if (osName === 'iOS' || osName === 'Android') setIsMobile(true)
-	}, [])
+		console.log(osName);
+		if (osName === "iOS" || osName === "Android") setIsMobile(true);
+	}, []);
 	return (
 		<VideoWrapper>
 			<div className="main-visual-inner">
@@ -62,19 +62,18 @@ const Landing = () => {
 			{isMobile ? (
 				<PostContainer />
 			) : (
-				<VideoBackground
-					muted
-					autoPlay
-					loop
-				>
+				<VideoBackground muted autoPlay loop>
 					<source
-						src={video}
+						// src={Video}
+						src={
+							"https://s3.ap-northeast-2.amazonaws.com/2023.05.galhome.webm/main.webm"
+						}
 						type="video/webm"
 					/>
 				</VideoBackground>
 			)}
 		</VideoWrapper>
-	)
-}
+	);
+};
 
-export default Landing
+export default Landing;
