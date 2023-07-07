02c89f27cad4af685b422f0ff59fb0a3603877da
jongsun <alterego1227@gmail.com>
2023-07-07 13:15:33 +0900

    수정작업중

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import IconFb from '../assets/image/common/ico-fb.svg'
-import IconYoutube from '../assets/image/common/ico-youtube.svg'
-import IconInsta from '../assets/image/common/ico-insta.svg'
-import IconIn from '../assets/image/common/ico-in.svg'
-import IconTw from '../assets/image/common/ico-tw.svg'
 import FooterLogo from '../assets/image/common/footer-logo.svg'
+import FooterYoutube from '../assets/image/common/footerYoutube.svg'
+import FooterBlog from '../assets/image/common/footerBlog.svg'
 
 const Container = styled.footer`
+	position: relative;
 	border-top: 1px solid #fff;
 `
 const PrivacyList = styled.ul`
@@ -27,73 +25,35 @@ const PrivacyList = styled.ul`
         // background-color: #fff;
         // opacity: 0.2;
       }
+	  @media (max-width: 919px) {
+		margin-bottom: 0.5rem !important;
+	  }
   }
+`
+
+const OutlinkList = styled.ul``
+
+const MiscContainer = styled.div`
+	@media (max-width: 919px) {
+		position: absolute;
+		left: 0;
+		top: 1rem;
+		right: 0;
+	}
+`
+
+const FooterInfo = styled.div`
+	@media (max-width: 919px) {
+		position: relative;
+		margin-top: 90px;
+	}
 `
 
 const Footer = () => {
 	return (
 		<div className="container p-4">
-			<div className="md:flex md:flex-row md:justify-end md:items-center mt-40">
-				<ul className="mb-3.5 gap-x-8 flex mt-5 md:mt-0 justify-end">
-					{/* <li>
-						<Link
-							to="/"
-							target="_blank"
-						>
-							<img
-								src={IconFb}
-								alt="페이스북"
-							/>
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/"
-							target="_blank"
-						>
-							<img
-								src={IconYoutube}
-								alt="유튜브"
-							/>
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/"
-							target="_blank"
-						>
-							<img
-								src={IconIn}
-								alt="링크드인"
-							/>
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/"
-							target="_blank"
-						>
-							<img
-								src={IconInsta}
-								alt="인스타그램"
-							/>
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/"
-							target="_blank"
-						>
-							<img
-								src={IconTw}
-								alt="트위터"
-							/>
-						</Link>
-					</li> */}
-				</ul>
-			</div>
 			<Container className="container text-center flex justify-between align-top md:text-left mb-20 pt-4">
-				<div>
+				<FooterInfo>
 					<img
 						src={FooterLogo}
 						className="footer-logo mb-3"
@@ -105,25 +65,53 @@ const Footer = () => {
 					>
 						2023 GALAXY corporation. CO., LTD. All rights reserved.
 					</p>
-				</div>
-				<PrivacyList className="text-sm flex md:justify-end">
-					<li>
-						<Link
-							to="/"
-							className="font-extralight"
-						>
-							개인정보처리방침
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/"
-							className="font-extralight"
-						>
-							문의하기
-						</Link>
-					</li>
-				</PrivacyList>
+				</FooterInfo>
+				<MiscContainer className="flex justify-between md:justify-normal">
+					<PrivacyList className="text-sm lg:flex md:mr-20 text-left">
+						<li>
+							<Link
+								to="/"
+								className="font-extralight"
+							>
+								개인정보처리방침
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/"
+								className="font-extralight"
+							>
+								문의하기
+							</Link>
+						</li>
+					</PrivacyList>
+					<OutlinkList className="flex">
+						<li className="mr-6">
+							<a
+								href="https://www.youtube.com"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<img
+									src={FooterYoutube}
+									alt="youtube"
+								/>
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.youtube.com"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<img
+									src={FooterBlog}
+									alt="youblog"
+								/>
+							</a>
+						</li>
+					</OutlinkList>
+				</MiscContainer>
 			</Container>
 		</div>
 	)
